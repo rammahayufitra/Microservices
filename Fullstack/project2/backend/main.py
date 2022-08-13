b305f9b81194bd3063fe9e1e07ab3de6fd8c5f66
rammafitra <rammahayufitra@gmail.com>
2022-08-13 21:37:25 +0700

    add

---
+++ Fullstack/project2/backend/main.py
@@ -0,0 +1,45 @@
+from shutil import disk_usage
+from flask import Flask, render_template 
+import psutil
+
+app = Flask(__name__)
+
+@app.route('/')
+def landingpage():
+    return render_template('landingPage.html')
+
+@app.route('/pieChart')
+def pieChart():
+    ram_usage = psutil.virtual_memory().percent
+    virtual_memory = psutil.virtual_memory().percent
+    swap_memory = psutil.swap_memory().percent
+    disk_usage = psutil.disk_usage('/').percent
+    cpu_usage = psutil.cpu_percent()
+    data = {
+        'ram_usage':str(ram_usage), 
+        'cpu_usage':str(cpu_usage), 
+        'virtual_memory':str(virtual_memory),
+        'swap_memory': str(swap_memory),
+        'disk_usage': str(disk_usage)
+    }
+    return data
+
+@app.route('/barChart')
+def barChart():
+    ram_usage = psutil.virtual_memory().percent
+    virtual_memory = psutil.virtual_memory().percent
+    swap_memory = psutil.swap_memory().percent
+    disk_usage = psutil.disk_usage('/').percent
+    cpu_usage = psutil.cpu_percent()
+    data = [
+        {'ram_usage':str(ram_usage), 
+        'cpu_usage':str(cpu_usage), 
+        'virtual_memory':str(virtual_memory),
+        'swap_memory': str(swap_memory),
+        'disk_usage': str(disk_usage)
+        }
+    ]
+    return data
+
+
+app.run()
---
+++ Fullstack/project2/frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import PieChart from './components/PieChart';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hardware Monitoring</h1>
+      <PieChart/>
+    </div>
+  );
+}
+export default App;
+
+      
